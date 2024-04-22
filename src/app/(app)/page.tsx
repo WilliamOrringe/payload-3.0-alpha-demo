c13a38214d5c5332752b888fb496b001250d814d
Will <williamorringe@gmail.com>
2024-04-22 18:59:04 +0100

    fix

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Page = () => {
   return (
     <>
-      <div className="flex flex-row gap-40">
+      <div className="flex flex-row gap-40 ">
         <Sidebar />
         <div className="grid grid-cols-5 w-full gap-16 mr-[18px] mt-20">
           {Array(55)
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,22 +6,22 @@ import Link from 'next/link'
 const Sidebar = () => {
   return (
     <>
-      <div className="relative w-[20%] flex ">
-        <span className="fixed rounded-lg bg-gray-500 dark:bg-shadedBackgroundDark shadow-good justify-center w-[20%] flex h-[90%]">
-          <div className="flex flex-col my-auto gap-10 font-good font-bold">
+      <div className="relative w-[10%] flex">
+        <span className="fixed rounded-lg bg-gray-500 dark:bg-shadedBackgroundDark shadow-good justify-center w-[10%] flex h-[90%]">
+          <div className="flex flex-col mt-10 gap-10 font-good font-bold">
             <Link href="/" className="flex flex-row gap-4">
               <FontAwesomeIcon icon={faHouseChimney} className="w-5 h-5" /> Home
             </Link>
-            <Link href="/" className="flex flex-row gap-4">
+            <Link href="/movies" className="flex flex-row gap-4">
               <FontAwesomeIcon icon={faFilm} className="w-5 h-5" /> Movies
             </Link>
-            <Link href="/" className="flex flex-row gap-4">
+            <Link href="/tv" className="flex flex-row gap-4">
               <FontAwesomeIcon icon={faTv} className="w-5 h-5" /> TV Shows
             </Link>
-            <Link href="/" className="flex flex-row gap-4">
-              <FontAwesomeIcon icon={faCalendar} className="w-5 h-5" /> Calendar
+            <Link href="/calendar" className="flex flex-row gap-4 ">
+              <FontAwesomeIcon icon={faCalendar} className="w-5 h-5 shadow-" /> Calendar
             </Link>
-            <Link href="/" className="flex flex-row gap-4">
+            <Link href="/account" className="flex flex-row gap-4">
               <FontAwesomeIcon icon={faUser} className="w-5 h-5" /> Account
             </Link>
           </div>
